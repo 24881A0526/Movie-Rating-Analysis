c505c55e682d86a236f3c1c9918ad7bb4f1126f5
24881A0526 <24881A0526@student.vardhaman.org>
2026-04-18 22:32:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # 1. Load your Movies file (Capital M as per your GitHub)
-movies = pd.read_csv('Movies.csv')
+movies = pd.read_csv('movie.csv')
 
 # 2. Load the small sample we just made (No compression errors!)
 ratings = pd.read_csv('ratings_sample.csv') 
